1f7fad331ae0c5a05c014358c0d89802afbed4a6
Arjun <arjunsah@sussol.net>
2019-01-07 17:43:58 +0545

    Created base, background and added logo for currency converter

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Login from './src/js/components/Login';
+// import Login from './src/js/components/Login';
+import Home from './src/js/screens/Home';
 
-const App = () => <Login />;
+EStyleSheet.build({
+  $primaryBlue: '#4F6D7A',
+  $white: '#FFFFFF',
+});
+
+const App = () => <Home />;
 
 export default App;
---
+++ src/js/components/Logo/Logo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  View, Text, ImageBackground, Image,
+} from 'react-native';
+
+import styles from './styles';
+
+const Logo = () => (
+  <View style={styles.container}>
+    <ImageBackground
+      resizeMode="contain"
+      style={styles.containerImage}
+      source={require('./images/background.png')}
+    >
+      <Image
+        resizeMode="contain"
+        style={styles.logo}
+        source={require('./images/logo.png')}
+      />
+      <Text style={styles.text}>Currency Converter</Text>
+    </ImageBackground>
+  </View>
+);
+
+export default Logo;
