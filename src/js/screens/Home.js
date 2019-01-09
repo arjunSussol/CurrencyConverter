355d01cdd09ca00287693f4f89b5899e32b5dfe0
Arjun <arjunsah@sussol.net>
2019-01-09 16:05:28 +0545

    Added Text to show LastConverted currency rate of current date

--- src/js/screens/Home.js
+++ src/js/screens/Home.js
@@ -5,11 +5,14 @@ import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Button';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_LAST_CONVERTED = new Date();
+const TEMP_CONVERSION_RATE = 0.79739;
 
 class Home extends Component {
   handleChangeText = () => {
@@ -46,6 +49,12 @@ class Home extends Component {
           buttonText={TEMP_QUOTE_CURRENCY}
           value={TEMP_QUOTE_PRICE}
           onPress={this.handlePressQuoteCurrency}
+        />
+        <LastConverted
+          date={TEMP_LAST_CONVERTED}
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          conversionRate={TEMP_CONVERSION_RATE}
         />
         <ClearButton
           onPress={this.handleSwapCurrency}
